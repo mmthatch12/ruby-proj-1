cea7b8a02d598f0c1f6f307109a15f752f09d276
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-11-26 17:32:42 -0700

    reformated candidates.rb

--- candidates.rb
+++ candidates.rb
@@ -1,32 +1,30 @@
-jane = { 
-    "first_name" => "Jane", 
-    "last_name" => "Doe",
-    level:  "senior_level",
-    "age" => 30,
-    "occupation" => "Software Developer",
-    "employed" => true
-    "current_salary" => 125000.00
-    "languages" => ["Ruby", "JavaScript", "Go"]
-}
+jane = {}
+jane["first_name"] = "Jane"
+jane["last_name"] = "Doe"
+jane{ level:  "senior_level" }
+jane["age"] = 30,
+jane["occupation"] = "Software Developer"
+jane["employed"] = true
+jane["current_salary"] = 125000.00
+jane["languages"] = ["Ruby", "JavaScript", "Go"]
 
-sara = { 
-    "first_name" => "Sara", 
-    "last_name" => "Smith",
-    level:  "mid_level",
-    "age" => 26,
-    "occupation" => "Software Developer",
-    "employed" => true
-    "current_salary" => 80000.00
-    "languages" => ["Ruby", "Python"]
-}
 
-jason = { 
-    "first_name" => "Jason", 
-    "last_name" => "Gray",
-    level:  "entry_level",
-    "age" => 23,
-    "occupation" => "Student",
-    "employed" => false
-    "current_salary" => 0.00
-    "languages" => ["C++", "Python"]
-}
+sara = {}
+sara["first_name"] = "Sara"
+sara["last_name"] = "Smith"
+sara{ level:  "mid_level" }
+sara["age"] = 26,
+sara["occupation"] = "Software Developer"
+sara["employed"] = true
+sara["current_salary"] = 80000.00
+sara["languages"] = ["Ruby", "Python"]
+
+jason = {}
+jason["first_name"] = "Jason"
+jason["last_name"] = "Gray"
+jason{ level:  "entry_level" }
+jason["age"] = 23,
+jason["occupation"] = "Student"
+jason["employed"] = false
+jason["current_salary"] = 0.00
+jason["languages"] = ["C++", "Python"]
