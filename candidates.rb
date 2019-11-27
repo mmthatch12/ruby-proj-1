72e59fcf8dc1efc6c9c5e91dc4ce4e94f8f1ed09
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-11-27 07:24:01 -0700

    finished candidates.rb

--- candidates.rb
+++ candidates.rb
@@ -1,30 +1,35 @@
 jane = {}
-jane["first_name"] = "Jane"
-jane["last_name"] = "Doe"
-jane{ level:  "senior_level" }
-jane["age"] = 30,
-jane["occupation"] = "Software Developer"
-jane["employed"] = true
-jane["current_salary"] = 125000.00
-jane["languages"] = ["Ruby", "JavaScript", "Go"]
+jane[:first_name] = "Jane"
+jane[:last_name] = "Doe"
+jane[:level] = "senior_level" 
+jane[:age] = 30,
+jane[:occupation] = "Software Developer"
+jane[:employed] = true
+jane[:current_salary] = 125000.00
+jane[:languages] = []
+jane[:languages] = "Ruby", "JavaScript", "Go"
 
 
 sara = {}
-sara["first_name"] = "Sara"
-sara["last_name"] = "Smith"
-sara{ level:  "mid_level" }
-sara["age"] = 26,
-sara["occupation"] = "Software Developer"
-sara["employed"] = true
-sara["current_salary"] = 80000.00
-sara["languages"] = ["Ruby", "Python"]
+sara[:first_name] = "Sara"
+sara[:last_name] = "Smith"
+sara[:level] = "mid_level"
+sara[:age] = 26
+sara[:occupation] = "Software Developer"
+sara[:employed] = true
+sara[:current_salary] = 80000.00
+sara[:languages] = []
+sara[:languages] = "Ruby", "Python"
 
 jason = {}
-jason["first_name"] = "Jason"
-jason["last_name"] = "Gray"
-jason{ level:  "entry_level" }
-jason["age"] = 23,
-jason["occupation"] = "Student"
-jason["employed"] = false
-jason["current_salary"] = 0.00
-jason["languages"] = ["C++", "Python"]
+jason[:first_name] = "Jason"
+jason[:last_name] = "Gray"
+jason[:level] = "entry_level"
+jason[:age] = 23
+jason[:occupation] = "Student"
+jason[:employed] = false
+jason[:current_salary] = 0.00
+jason[:languages] = []
+jason[:languages] = "C++", "Python"
+
+candidates = [jane, sara, jason]
